d75536b1372f3c748962887e185d96e4d43a3f87
Stef <stbuwalda@gmail.com>
2025-04-18 17:14:42 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 from wordcloud import WordCloud  # type: ignore
 
 wordcloud = WordCloud(
-    width=1920,
-    height=1080,
+    width=800,
+    height=400,
     background_color="black",  # or 'black', or any HTML color
     colormap="viridis",  # matplotlib colormap ('plasma', 'cool', 'inferno')
     # font_path="path/to/font.ttf",  # Use a custom font
     max_words=100,  # Max number of words to include
     min_font_size=10,
-    max_font_size=200,
+    max_font_size=100,
     prefer_horizontal=1,  # Between 0 (all vertical) and 1 (all horizontal)
     scale=4,  # Higher = better resolution
     contour_color="steelblue",  # Outline color (when using contour_width)
